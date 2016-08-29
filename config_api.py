d9a898fd2d66b1ad9b0be9c5748f845cc1317053
huanglijie <huanglijie@NICA.localdomain>
2016-08-29 09:15:18 +0000

    add config and api file

---
+++ config_api.py
@@ -0,0 +1,32 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import sys
+import os
+import time
+import ConfigParser
+
+class Config:
+    def __init__(self, path):
+        """Config instance initialization."""
+        self.path = path
+        self.cf = ConfigParser.ConfigParser()
+        self.cf.read(self.path)
+
+    def get(self, field, key):
+        """Get config value."""
+        try:
+            result = self.cf.get(field, key)
+        except:
+            result = ''
+        return result
+
+    def set(self, field, key, value):
+        """Set config value."""
+        try:
+            self.cf.set(field, key, value)
+            cf.write(open(self.path, 'w'))
+        except:
+            return False
+        return True
+
