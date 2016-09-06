43d577c1d3cf10c5b403e5159c1f7fc80105a7db
Lijie Huang <huanglijie@outlook.com>
2016-09-06 22:18:24 +0800

    refactor done

--- brainDecTool/sample/stimuli_mat2png.py
+++ brainDecTool/sample/stimuli_mat2png.py
@@ -4,18 +4,22 @@
 import os
 import numpy as np
 from scipy.misc import imsave
-from brainDecodingToolbox import datasugar as sugar
 
-base_dir = r'/home/huanglijie/workingdir/brainDecoding'
+from brainDecTool.io import hdf5 as bdio
+from brainDecTool.util import configParser
 
-tf = sugar.open_mat(os.path.join(base_dir, 'Stimuli.mat'), env='dev')
+# config parser
+cf = configParser.Config('config')
+data_dir = cf.get('base', 'path')
+
+tf = bdio.open_hdf5(os.path.join(data_dir, 'Stimuli.mat'))
 #tf.listNodes
 stimages = tf.get_node('/st')[:]
 svimages = tf.get_node('/sv')[:]
 tf.close()
 
 # save training stimuli as png file
-for i in stimages.shape[0]:
+for i in range(stimages.shape[0]):
     x = stimages[i, :]
     new_x = np.zeros((x.shape[1], x.shape[2], 3), dtype=np.uint8)
     new_x[..., 0] = x[0, ...]
@@ -26,7 +30,7 @@ for i in stimages.shape[0]:
     imsave(file_name, new_x)
 
 # save validation stimuli as png file
-for i in svimages.shape[0]:
+for i in range(svimages.shape[0]):
     x = svimages[i, :]
     new_x = np.zeros((x.shape[1], x.shape[2], 3), dtype=np.uint8)
     new_x[..., 0] = x[0, ...]
---
+++ brainDecTool/util/README.md
@@ -0,0 +1,3 @@
+# Data sugar
+For your convenience, the `config` and datasugar script can be used to set the path of data directory.
+While using this util, the `config` file should be modifued for your work and placed in your own data directory.
