3ef99c24fe5188b29bc10ba7a62438ab7fe4d212
Lijie Huang <huanglijie@outlook.com>
2016-09-09 22:19:09 +0800

    add function for bold data loading

---
+++ brainDecTool/sample/bold_load.py
@@ -0,0 +1,21 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+import numpy as np
+
+from brainDecTool.io import hdf5 as bdio
+from brainDecTool.util import configParser
+
+# config parser
+cf = configParser.Config('config')
+data_dir = cf.get('base', 'path')
+
+tf = bdio.open_hdf5(os.path.join(data_dir, 'VoxelResponses_subjecr1.mat'))
+tf.listNodes
+data = tf.get_node('/rt')[:]
+roi = tf.get_node('/roi/v1lh')[:].flatten()
+v1lh_idx = np.nonzero(roi==1)[0]
+v1lh_resp = data[v1lh_idx]
+tf.close()
+
