5930894df2b8d6bb870e892bfe6bbf67674362bd
Lijie Huang <huanglijie@outlook.com>
2016-09-14 17:05:29 +0800

    update

--- brainDecTool/sample/bold_load.py
+++ brainDecTool/sample/bold_load.py
@@ -5,7 +5,6 @@ import os
 import numpy as np
 import tables
 
-from brainDecTool.io import hdf5 as bdio
 from brainDecTool.util import configParser
 
 # config parser
@@ -13,7 +12,24 @@ cf = configParser.Config('config')
 data_dir = cf.get('base', 'path')
 
 tf = tables.open_file(os.path.join(data_dir, 'VoxelResponses_subjecr1.mat'))
-tf.listNodes
+tf.list_nodes
+
+# merge v1 and v2 in both hemisphere
+l_v1_roi = tf.get_node('/roi/v1lh')[:]
+#r_v1_roi = tf.get_node('/roi/v1rh')[:]
+#l_v2_roi = tf.get_node('/roi/v2lh')[:]
+#r_v2_roi = tf.get_node('/roi/v2rh')[:]
+
+# l_v1 range: 
+#   i(A-P): 0 ~ 14
+#   j(I-S): 11 ~ 28
+#   k(R-L): 31 ~ 39
+# r_v1 range:
+#   i: 0 ~ 17
+#   j: 13 ~ 27
+#   k: 23 ~ 31
+tmp_l_v1 = np.zeros((l_v1_roi.shape[]))
+
 data = tf.get_node('/rt')[:]
 roi = tf.get_node('/roi/v1lh')[:].flatten()
 v1lh_idx = np.nonzero(roi==1)[0]
