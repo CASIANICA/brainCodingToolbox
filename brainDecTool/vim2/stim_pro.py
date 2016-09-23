c38bb30e0b8bdf1a6dc017e1508b954583219486
Lijie Huang <huanglijie@outlook.com>
2016-09-23 22:22:50 +0800

    add script for stimulus processing

---
+++ brainDecTool/vim2/stim_pro.py
@@ -0,0 +1,19 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+import numpy as np
+
+from brainDecTool.util import configParser
+import util as vutil
+
+# config parser
+cf = configParser.Config('config')
+data_dir = cf.get('base', 'path')
+stim_dir = os.path.join(data_dir, 'stimulus_val')
+
+# load stimulus time courses
+data = np.load(os.path.join(stim_dir, 'feat1_sti_val.npy'), mmap_mode='r')
+# read time course from first node
+tc = data[:, 0]
+
