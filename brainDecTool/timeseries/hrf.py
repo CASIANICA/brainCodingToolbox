9f50b10491648c3d73c863dd9b212fdbf8652440
Lijie Huang <huanglijie@outlook.com>
2016-09-23 23:27:22 +0800

    add convolution processing

--- brainDecTool/timeseries/hrf.py
+++ brainDecTool/timeseries/hrf.py
@@ -4,9 +4,10 @@
 import numpy as np
 from scipy.stats import gamma
 
-bi_gamma_hrf(times):
+def biGammaHRF(times):
     """Return values for HRF at given times.
     This HRF is derived from the sum of two Gamma function.
+    Unit of time: second.
     """
     # Gamma pdf for the peak
     peak_values = gamma.pdf(times, 6)
@@ -16,4 +17,13 @@ bi_gamma_hrf(times):
     values = peak_values - 0.35 * undershoot_values
     # Scale max to 0.6
     return values / np.max(values) * 0.6
+
+def biGammaHRFSignal(end_time, unit):
+    """Return a time course from time 0 to `end_time` generated from
+    the biGammaHRF.
+    Time unit is second. The time resolution is specified with
+    parameter `unit`, for example, 1ms -> 0.001.
+    """
+    hrf_times = np.arange(0, end_time, unit)
+    return biGammaHRF(hrf_times)
 
--- brainDecTool/vim2/stim_pro.py
+++ brainDecTool/vim2/stim_pro.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 
 from brainDecTool.util import configParser
+from brainDecTool.timeseries import hrf
 import util as vutil
 
 # config parser
@@ -17,4 +18,8 @@ data = np.load(os.path.join(stim_dir, 'feat1_sti_val.npy'), mmap_mode='r')
 # read time course from first node
 # data.shape = (8100, 290400) for validation dataset.
 tc = data[:, 0]
+# convolve with HRF
+# TODO: Caution of TIME RESOLUTION
+hrf_signal = biGammaHRFSignal(20, 0.04);
+con_tc = np.convolve(tc, hrf_signal)
 
