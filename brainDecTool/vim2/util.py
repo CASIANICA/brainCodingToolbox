4f5e7bdf694766795460a9b6963d34765a0a8a44
Lijie Huang <huanglijie@outlook.com>
2016-10-10 22:37:59 +0800

    update

--- brainDecTool/vim2/util.py
+++ brainDecTool/vim2/util.py
@@ -51,7 +51,7 @@ def coord2angle(pos_mtx):
     for i in range(row_num):
         uvtr = unit_vector(vtr[i])
         usvtr = unit_vector(std_vtr)
-        if uvtr[0] < 0:
+        if vtr[i][0] < 27:
             flag = -1
         else:
             flag = 1
