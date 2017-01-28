5aad789d983c4c04149bfeb7af27c93b523055ca
Lijie Huang <huanglijie@outlook.com>
2017-01-28 20:08:53 +0800

    update

--- brainDecTool/pipeline/base.py
+++ brainDecTool/pipeline/base.py
@@ -1,6 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
+import os
 import numpy as np
 from joblib import Parallel, delayed
 from brainDecTool.math import corr2_coef, ols_fit, ridge
