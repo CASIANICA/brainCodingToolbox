c6ceeeff6c22def0930b8021fd44b83b5620999d
Lijie Huang <huanglijie@outlook.com>
2017-06-14 23:19:08 +0800

    convert dist to angle in retinotopic mapping

--- brainDecTool/pipeline/retinotopy.py
+++ brainDecTool/pipeline/retinotopy.py
@@ -4,30 +4,25 @@
 import numpy as np
 from brainDecTool.math import unit_vector
 
-def coord2ecc(pos_mtx, image_size):
-    """Return the distance to the image center.
-    image_size : size (height,  width) of the stimulus.
+def coord2ecc(pos_mtx, img_size, max_visual_angle):
+    """Return the distance to the image center in unit of degree (visual angle).
+    image_size : size of the stimulus.
     """
-    if not isinstance(image_size, tuple):
-        print 'A tuple is required for `image_size`'
-        return
-
-    center_pos = [image_size[0]*1.0/2-0.5, image_size[1]*1.0/2-0.5]
+    center_pos = [img_size/2.0-0.5, img_size/2.0-0.5]
     row_num = pos_mtx.shape[0]
     cen = np.repeat([center_pos], row_num, axis=0)
     dist = np.linalg.norm(pos_mtx-cen, axis=1)
-    return dist
+    L = img_size / 2.0
+    A = np.tan(max_visual_angle/360.0*np.pi)
+    ecc = np.arctan(A*dist/L) / np.pi * 180
+    return ecc
 
-def coord2angle(pos_mtx, image_size):
+def coord2angle(pos_mtx, img_size):
     """Return the angle given a coordinate in the image.
-    image_size : size (height, width) of the stimulus.
+    image_size : size of the stimulus.
     """
-    if not isinstance(image_size, tuple):
-        print 'A tuple is required for `image_size`'
-        return
-
-    std_pos = [0, image_size[1]*1.0/2-0.5]
-    center_pos = [image_size[0]*1.0/2-0.5, image_size[1]*1.0/2-0.5]
+    std_pos = [0, img_size/2.0-0.5]
+    center_pos = [img_size/2.0-0.5, img_size/2.0-0.5]
     std_vtr = np.array(std_pos) - np.array(center_pos)
     row_num = pos_mtx.shape[0]
     cen = np.repeat([center_pos], row_num, axis=0)
@@ -42,4 +37,24 @@ def coord2angle(pos_mtx, image_size):
         else:
             ang[i] = a
     return ang
+
+def get_prf_size(pos_mtx, img_size, max_visual_angle):
+    """Get pRF size based on pRF parameters.
+    `pos_mtx` must be a n x 3 array, which the first two columns are pRF
+    center position (row x column) in visual stimuli, and the 3rd column
+    is the size of the pRF in pixels (i.e. FWHM).
+    The size of pRF is returned in unit of degree (visual angle).
+    """
+    center_pos = [img_size/2.0-0.5, img_size/2.0-0.5]
+    row_num = pos_mtx.shape[0]
+    cen = np.repeat([center_pos], row_num, axis=0)
+    center_dist = np.linalg.norm(pos_mtx[:, :2]-cen, axis=1)
+    max_dist = center_dist + pos_mtx[:, 2]/2.0
+    min_dist = center_dist - pos_mtx[:, 2]/2.0
+    L = img_size / 2.0
+    A = np.tan(max_visual_angle/360.0*np.pi)
+    max_ecc = np.arctan(A*max_dist/L) / np.pi * 180
+    min_ecc = np.arctan(A*min_dist/L) / np.pi * 180
+    return max_ecc - min_ecc
+
 
