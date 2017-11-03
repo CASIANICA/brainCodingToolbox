df170051519cb13130b2c58e0b1ebbd79eb04ef8
Lijie Huang <huanglijie@outlook.com>
2017-11-03 16:06:56 +0800

    update

--- braincode/vim2/quantitative_prf.py
+++ braincode/vim2/quantitative_prf.py
@@ -47,14 +47,49 @@ def load_roi_prf(roi_dir):
 
     return orig_data, df
 
-def plot_gabor_contrib(roi, df):
+def plot_gabor_contrib(prf_dir, roi, lhmerged=True):
     """Plot tunning contribution of each gabor sub-banks VS. ecc."""
+    df_list = []
+    hemis = ['lh', 'rh']
+    hemi_flags = [1, 1]
+    for hemi in hemis:
+        roi_dir = os.path.join(prf_dir, roi+hemi)
+        if not os.path.exists(roi_dir):
+            print '%s data does not exists'%(roi+hemi)
+            hemi_flags[hemis.index(hemi)] = 0
+            continue
+        # if roi_dir exists, load prf data
+        ecc = np.load(roi_dir, 'ecc.npy')
+        gabor_corr = np.load(roi_dir, 'gabor_contributes.npy')
+        val_corr = np.load(roi_dir, 'reg_sel_model_corr.npy')
+        # select significantly predicted voxels
+        thr = 0.17
+        sel_vxl = val_corr>=thr
+        hemi_dict = {'corr': gabor_corr[sel_vxl, :].flatten(),
+                     'frequenct': [1, 2, 3, 4, 5] * sel_vxl.sum(),
+                     'ecc': np.repeat(ecc[sel_vxl], 5)}
+        df_list.append(pd.DataFrame(hemi_dict))
+    if not np.sum(hemi_flags):
+        print 'No roi found'
+        return
+    # plot
     sns.set(color_codes=True)
-    for i in range(5):
-        g = sns.lmplot(x='ecc', y='wt_sum_%s'%(i+1), data=df, x_bins=10,
-                       size=6, order=2)
-        g.set(xlim=(0, 14), ylim=(-0.3, 0.8))
-        g.savefig('gabor_contrib_%s_sp%s.png'%(roi, i+1))
+    if lh_merged and np.sum(hemi_flags)==2:
+        df = pd.concat(df_list)
+        g = sns.lmplot(x='ecc', y='corr', hue='frequency', col='frequency',
+                       data=df, order=2, x_bins=np.arange(1, 11, 1))
+        g.savefig('%s_gabor_contrib.png'%(roi))
+    else:
+        if np.sum(hemi_flags)==2:
+            for df in df_list:
+                g = sns.lmplot(x='ecc', y='corr', hue='frequency',
+                               col='frequency', data=df, order=2,
+                               x_bins=np.arange(1, 11, 1))
+                g.savefig('%s_gabor_contrib.png'%(roi+hemis[df_list.index(df)]))
+        else:
+            g = sns.lmplot(x='ecc', y='corr', hue='frequency', col='frequency',
+                           data=df, order=2, x_bins=np.arange(1, 11, 1))
+            g.savefig('%s_gabor_contrib.png'%(roi+hemis[hemi_flags.index(1)]))
 
 def load_gabor_contrib(prf_dir):
     """Load tunning contribution of different gabor sub-banks."""
