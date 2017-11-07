a2b91c82036d520cddcacdb1720857e34b3a2fd1
Lijie Huang <huanglijie@outlook.com>
2017-11-07 17:18:59 +0800

    update

--- braincode/prf/dataio.py
+++ braincode/prf/dataio.py
@@ -42,3 +42,32 @@ def load_vim2_fmri(db_dir, subj_id, roi=None):
     tf.close()
     return vxl_idx, train_ts, val_ts
 
+def load_vim1_fmri(db_dir, subj_id, roi=None):
+    """Load fmri time courses for each voxel within specified ROI."""
+    fmri_file = os.path.join(db_dir, 'EstimatedResponses.mat')
+    tf = tables.open_file(fmri_file)
+    # create mask
+    # train data shape: (1750, ~25000)
+    train_ts = tf.get_node('/dataTrnS%s'%(subj_id))[:]
+    train_ts = train_ts.T
+    # get non-NaN voxel index
+    fmri_s = train_ts.sum(axis=1)
+    non_nan_idx = np.nonzero(np.logical_not(np.isnan(fmri_s)))[0]
+    if roi:
+        rois = tf.get_node('/roiS%s'%(subj_id))[:]
+        rois = rois[0]
+        roi_idx = {'v1': 1, 'v2': 2, 'v3': 3, 'v3a': 4,
+                   'v3b': 5, 'v4': 6, 'LO': 7}
+        vxl_idx = np.nonzero(rois==roi_idx[roi])[0]
+        vxl_idx = np.intersect1d(vxl_idx, non_nan_idx)
+    else:
+        vxl_idx = non_nan_idx
+    # load fmri response
+    # data shape: (#voxel, 7200/540)
+    train_ts = np.nan_to_num(train_ts[vxl_idx])
+    val_ts = tf.get_node('/dataValS%s'%(subj_id))[:]
+    val_ts = val_ts.T
+    val_ts = np.nan_to_num(val_ts[vxl_idx])
+    tf.close()
+    return vxl_idx, train_ts, val_ts
+
