3db1d36dc2f2fb1abc3bd7138d7a7f5d0a6ca26c
Lijie Huang <huanglijie@outlook.com>
2017-11-09 20:19:34 +0800

    update

--- braincode/prf/vim1_pro.py
+++ braincode/prf/vim1_pro.py
@@ -8,7 +8,9 @@ import bob.ip.gabor
 from joblib import Parallel, delayed
 
 from braincode.util import configParser
-from braincode.math import make_2d_gaussian
+from braincode.math import make_2d_gaussian, ridge
+from brain.math.norm import zscore
+from brain.prf import dataio
 
 
 def get_gabor_features(img):
@@ -130,34 +132,100 @@ def model_pro(feat, cand_model, xi, yi, si):
         cand_model[mi, idx_head:(idx_head+10), ...] = res.reshape(10, 72)
         idx_head += 10
 
-def stim_downsample():
-    """Stimuli processing."""
-    #db_dir = r'/home/huanglijie/workingdir/brainDecoding/vim1/gabor_features'
-    #prefix = {'train': 'Stimuli_Trn_FullRes', 'val': 'Stimuli_Val_FullRes'}
+def ridge_fitting(feat_dir, prf_dir, db_dir, subj_id, roi):
+    """pRF model fitting using ridge regression.
+    90% trainning data used for model tuning, and another 10% data used for
+    model seletion.
+    """
+    # load fmri response
+    vxl_idx, train_fmri_ts, val_fmri_ts = dataio.load_vim1_fmri(db_dir, subj_id,
+                                                                roi=roi)
+    del val_fmri_ts
+    print 'Voxel number: %s'%(len(vxl_idx))
+    # load candidate models
+    train_models = np.load(os.path.join(feat_dir, 'train_candidate_model.npy'),
+                           mmap_mode='r')
+    # output directory config
+    roi_dir = os.path.join(prf_dir,  roi)
+    check_path(roi_dir)
+    # model seletion and tuning
+    ALPHA_NUM = 20
+    BOOTS_NUM = 15
+    paras_file = os.path.join(roi_dir, 'reg_paras.npy')
+    paras = np.memmap(paras_file, dtype='float64', mode='w+',
+                      shape=(42500, len(vxl_idx), 72))
+    mcorr_file= os.path.join(roi_dir, 'reg_model_corr.npy')
+    mcorr = np.memmap(mcorr_file, dtype='float64', mode='w+',
+                      shape=(42500, len(vxl_idx)))
+    alphas_file = os.path.join(roi_dir, 'reg_alphas.npy')
+    alphas = np.memmap(alphas_file, dtype='float64', mode='w+',
+                       shape=(42500, len(vxl_idx)))
+    # fMRI data z-score
+    print 'fmri data temporal z-score'
+    m = np.mean(train_fmri_ts, axis=1, keepdims=True)
+    s = np.std(train_fmri_ts, axis=1, keepdims=True)
+    train_fmri_ts = (train_fmri_ts - m) / (1e-10 + s)
+    # split training dataset into model tunning set and model selection set
+    tune_fmri_ts = train_fmri_ts[:, :int(1750*0.9)]
+    sel_fmri_ts = train_fmri_ts[:, int(1750*0.9):]
+    # model testing
+    for i in range(42500):
+        print 'Model %s'%(i)
+        train_x = np.array(train_models[i, ...]).astype(np.float64)
+        # TODO: modify this line
+        train_x = zscore(train_x.T).T
+        # split training dataset into model tunning and selection sets
+        tune_x = train_x[:int(1750*0.9), :]
+        sel_x = train_x[int(1750*0.9):, :]
+        wt, r, alpha, bscores, valinds = ridge.bootstrap_ridge(
+                tune_x, tune_fmri_ts.T, sel_x, sel_fmri_ts.T,
+                alphas=np.logspace(-2, 3, ALPHA_NUM),
+                nboots=BOOTS_NUM, chunklen=175, nchunks=1,
+                single_alpha=False, use_corr=False)
+        paras[i, ...] = wt.T
+        mcorr[i] = r
+        alphas[i] = alpha
+    # save output
+    paras = np.array(paras)
+    np.save(paras_file, paras)
+    mcorr = np.array(mcorr)
+    np.save(mcorr_file, mcorr)
+    alphas = np.array(alphas)
+    np.save(alphas_file, alphas)
 
-    #data_type = 'val'
-
-    #if data_type == 'train':
-    #    for i in range(15):
-    #        src_file = os.path.join(db_dir,
-    #                        prefix['train']+'_%02d_gabor_features.npy'%(i+1))
-    #        print 'Load file %s ...'%(src_file)
-    #        imgs = np.load(src_file)
-    #        # output matrix: row x col x image-number
-    #        print 'image size %s'%(imgs.shape[3])
-    #        out_imgs = imgs[..., 0:8, :].sum(axis=2)
-    #        out_file = prefix['train']+'_%02d_smallest_gabor_features.npy'%(i+1)
-    #        np.save(out_file, out_imgs)
-    #else:
-    #    src_file = os.path.join(db_dir, prefix['val']+'_gabor_features.npy')
-    #    print 'Load file %s ...'%(src_file)
-    #    imgs = np.load(src_file)
-    #    # output matrix: row x col x image-number
-    #    print 'image size %s'%(imgs.shape[3])
-    #    out_imgs = imgs[..., 0:8, :].sum(axis=2)
-    #    out_file = prefix['val']+'smallest_gabor_features.npy'
-    #    np.save(out_file, out_imgs)
-    pass
+def prf_selection(feat_dir, prf_dir, db_dir, subj_id, roi):
+    """Select best model for each voxel and validating."""
+    # load fmri response
+    vxl_idx, train_fmri_ts, val_fmri_ts = dataio.load_vim1_fmri(db_dir, subj_id,
+                                                                roi=roi)
+    del train_fmri_ts
+    print 'Voxel number: %s'%(len(vxl_idx))
+    # load candidate models
+    val_models = np.load(os.path.join(feat_dir, 'val_candidate_model.npy'),
+                         mmap_mode='r')
+    # output directory config
+    roi_dir = os.path.join(prf_dir, roi)
+    # load candidate model parameters 
+    paras = np.load(os.path.join(roi_dir, 'reg_paras.npy'))
+    mcorr = np.load(os.path.join(roi_dir, 'reg_model_corr.npy'))
+    alphas = np.load(os.path.join(roi_dir, 'reg_alphas.npy'))
+    sel_paras = np.zeros((mcorr.shape[1], 72))
+    sel_model = np.zeros(mcorr.shape[1])
+    sel_model_corr = np.zeros(mcorr.shape[1])
+    for i in range(mcorr.shape[1]):
+        maxi = np.argmax(mcorr[:, i])
+        print 'Voxel %s - Max corr %s - Model %s'%(i, mcorr[maxi, i], maxi)
+        print 'Alpha : %s'%(alphas[maxi, i])
+        sel_paras[i] = paras[maxi, i]
+        sel_model[i] = maxi
+        feats = np.array(val_models[maxi, ...]).astype(np.float64)
+        feats = zscore(feats.T).T
+        pred = np.dot(feats, sel_paras[i])
+        sel_model_corr[i] = np.corrcoef(pred, val_fmri_ts[i])[0, 1]
+        print 'Val Corr : %s'%(sel_model_corr[i])
+    np.save(os.path.join(roi_dir, 'reg_sel_paras.npy'), sel_paras)
+    np.save(os.path.join(roi_dir, 'reg_sel_model.npy'), sel_model)
+    np.save(os.path.join(roi_dir, 'reg_sel_model_corr.npy'), sel_model_corr)
 
 if __name__ == '__main__':
     """Main function."""
@@ -173,5 +241,17 @@ if __name__ == '__main__':
     # get gabor features
     #get_stim_features(db_dir, feat_dir, 'train')
     # get candidate models
-    get_candidate_model(feat_dir, 'val')
+    #get_candidate_model(feat_dir, 'val')
+
+    #-- general config
+    subj_id = 1
+    roi = 'v1'
+    # directory config
+    subj_dir = os.path.join(res_dir, 'vim1_S%s'%(subj_id))
+    prf_dir = os.path.join(subj_dir, 'prf')
+
+    #-- pRF model fitting
+    # pRF model tunning
+    ridge_fitting(feat_dir, prf_dir, db_dir, subj_id, roi)
+    #prf_selection(feat_dir, prf_dir, db_dir, subj_id, roi)
 
