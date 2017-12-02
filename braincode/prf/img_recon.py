09e01a1b911c9d73dcb76710c7bc8d09e05d617f
Lijie Huang <huanglijie@outlook.com>
2017-12-02 11:02:14 +0800

    add new func

---
+++ braincode/prf/img_recon.py
@@ -0,0 +1,10 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+
+import os
+import numpy as np
+import tensorflow as tf
+
+from braincode.util import configParser
+
+
