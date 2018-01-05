f218182d3ea85f68a6363de82e8cc7bff31db3cf
Lijie Huang <huanglijie@outlook.com>
2018-01-05 16:40:46 +0800

    update

--- braincode/prf/colormap.py
+++ braincode/prf/colormap.py
@@ -7,6 +7,16 @@ from matplotlib.colors import colorConverter
 
 
 def get_overlay_colormap():
+    color1 = colorConverter.to_rgba('red')
+    color2 = colorConverter.to_rgba('yellow')
+    maskcmp = mpl.colors.LinearSegmentedColormap.from_list('maskcolor',
+                                                [color1, color2], 256)
+    maskcmp._init()
+    maskcmp._lut[:15, -1] = 0
+    maskcmp._lut[15:, -1] = 0.45
+    return maskcmp
+
+def get_mask_colormap():
     color1 = colorConverter.to_rgba('yellow')
     color2 = colorConverter.to_rgba('red')
     maskcmp = mpl.colors.LinearSegmentedColormap.from_list('maskcolor',
